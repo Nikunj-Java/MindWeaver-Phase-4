b2e252334ce0baf094eff1c60713e714658e6833
unknown <ganitniduniyanikunj10@gmail.com>
2022-08-26 15:55:42 +0530

    Angular Component, Bindings

--- Session-4/myapp/src/app/binding/binding.component.ts
+++ Session-4/myapp/src/app/binding/binding.component.ts
@@ -15,4 +15,13 @@ export class BindingComponent implements OnInit {
   name:string ="NIKUNJ SONI";
   url:string="http://www.google.com";
 
+  products=[{name:"pencile",price:"8.3",available:"26-08-2022",ratings:4.8},
+  {name:"pen",price:"8.8",available:"27-08-2022",ratings:5.0},
+  {name:"eraser",price:"5.5",available:"31-08-2022",ratings:4.2},
+  {name:"scale",price:"3.3",available:"01-09-2022",ratings:4.1},
+  {name:"books",price:"10",available:"05-09-2022",ratings:4.9}
+
+
+  ]
+
 }
