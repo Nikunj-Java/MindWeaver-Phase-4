f0f0eae0ae4f2f30c7d7cce81bf27778bb15fc82
unknown <ganitniduniyanikunj10@gmail.com>
2022-08-30 16:14:01 +0530

    Pipes & Routing

---
+++ Session-5/routingapp/src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+
+  //path to redirect
+  {"path":"about",component:AboutusComponent},
+  {"path":"home",component:HomeComponent},
+  {"path":"contact",component:ContactusComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
