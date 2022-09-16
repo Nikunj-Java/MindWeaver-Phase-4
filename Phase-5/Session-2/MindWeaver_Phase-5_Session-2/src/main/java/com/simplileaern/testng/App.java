908524632e6e032897421e28b751f3b4fda2b3a9
unknown <ganitniduniyanikunj10@gmail.com>
2022-09-16 15:43:12 +0530

    Registartion Form,TestNg

---
+++ Phase-5/Session-2/MindWeaver_Phase-5_Session-2/src/main/java/com/simplileaern/testng/App.java
@@ -0,0 +1,13 @@
+package com.simplileaern.testng;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
